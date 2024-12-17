99bd033b15798ea19fc02a4a264d136ae0bbbba5
ivank101 <133937096+ivank101@users.noreply.github.com>
2024-12-17 21:30:08 +0900

    Add files via upload

---
+++ Min/lang_login.js
@@ -0,0 +1,79 @@
+const languageData = {};
+const loadLanguageData = async (lang) => {
+    try {
+      const response = await fetch(`../Min/json/${lang}.json`);
+      const data = await response.json();
+      languageData[lang] = data;
+    } catch (error) {
+      console.error(error);
+      // Hiển thị thông báo lỗi cho người dùng
+    }
+};
+function getLanguageText(lang) {
+    switch (lang) {
+        case 'en':
+            return 'English';
+        case 'vi':
+            return 'Tiếng Việt';
+        case 'jp':
+            return '日本語';
+        case 'indo':
+            return 'Indonesia';
+        default:
+            return 'Select Language';
+    }
+}
+// Hàm thiết lập ngôn ngữ
+function setLanguage(lang) {
+    localStorage.setItem('selectedLang', lang);
+    updateLanguageContent(lang);
+
+    const dropdownButton = document.querySelector('.dropdown-toggle');
+    dropdownButton.textContent = getLanguageText(lang);
+}
+
+// Update the UI content with the current language
+const updateLanguageContent = () => {
+    const loginData = languageData[currentLanguage]['login'];
+
+    const titleEl = document.querySelector("h2");
+    const subtitleEl = document.querySelector(".specific-subtitle");
+    const registerLink = document.querySelector(".new-register");
+    const emailInput = document.querySelector("input[name='email']");
+    const passwordInput = document.querySelector("input[name='password']");
+    const rememberPasswordLabel = document.querySelector("label[for='pass_save']");
+    const loginBtn = document.querySelector("button[name='login']");
+
+    const defaultLink = registerLink.getAttribute("data-lang-link");
+    registerLink.textContent = loginData.registerText;
+    registerLink.href = defaultLink;
+
+    titleEl.textContent = loginData.title;
+    subtitleEl.textContent = loginData.subtitle;
+    emailInput.placeholder = loginData.emailPlaceholder;
+    passwordInput.placeholder = loginData.passwordPlaceholder;
+    rememberPasswordLabel.textContent = loginData.rememberPasswordLabel;
+    loginBtn.textContent = loginData.loginButton;
+
+    
+
+};
+
+
+// Attach event listener to dropdown menu items
+const dropdownItems = document.querySelectorAll(".change-lang");
+dropdownItems.forEach((item) => item.addEventListener("click", async (event) => {
+    currentLanguage = event.target.dataset.lang;
+    if (!languageData[currentLanguage]) {
+        console.log(currentLanguage)
+        await loadLanguageData(currentLanguage);
+        setLanguage(currentLanguage);
+        updateLanguageContent(); // Gọi hàm cập nhật nội dung sau khi dữ liệu đã được tải xong
+    } else {
+        updateLanguageContent();
+    }
+}));
+
+
+  
+  
---
+++ Min/login.php
@@ -0,0 +1,71 @@
+<?php
+session_start();
+require_once dirname(__FILE__) . '../function/db_connection.php';
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "papermate";
+// require_once dirname(__FILE__) . '../function/auto_login.php';
+//Connect to the database
+$conn = connection();
+$message = '';
+
+// Function to check login and redirect users
+function login()
+{
+  // Check if the "Remember Me" checkbox is checked
+  if (isset($_POST['pass_save'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    // Store the email and password in the session
+    $_SESSION['remember_email'] = $email;
+    $_SESSION['remember_password'] = $password;
+  } else {
+    // If the checkbox is not checked, unset the session variables
+    unset($_SESSION['remember_email']);
+    unset($_SESSION['remember_password']);
+    unset($_SESSION['auto_login_expiration']);
+    unset($_SESSION['logout']);
+  }
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    if (!empty($email) && !empty($password)) {
+      $conn = connection();
+
+      
+      // Query the user table
+      $userQuery = "SELECT * FROM user WHERE EMAIL = :email";
+      $userStmt = $conn->prepare($userQuery);
+      $userStmt->bindParam(':email', $email);
+      $userStmt->execute();
+      $userResult = $userStmt->fetch(PDO::FETCH_ASSOC);
+
+      if ($userResult) {
+        // User with email exists in the user table
+        // Check the password
+        if (count($userResult) > 0 && password_verify($password, $userResult['password'])) {
+          // Successful login with user account
+          // Save information to session or perform other actions
+          $_SESSION['userid'] = $userResult['userid'];
+          header("Location: ../Ivan/home.php");
+          exit();
+        } else {
+          // echo "Incorrect password!";
+        }
+      } else {
+        $message = 'User does not exist!';
+      }
+      $conn = null;
+    } else {
+      echo "Please enter email and password!";
+    }
+  }
+}
+
+// Call the login function
+login();
+?>
